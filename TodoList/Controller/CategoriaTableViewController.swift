00b2589923a6672072cdb33ca6945b2a8b97814d
Gonzalo Trujillo Vallejo <4bctrujillog1112@gmail.com>
2023-01-28 16:45:25 +0100

    CategoriaTableViewController implemented

--- TodoList/Controller/CategoriaTableViewController.swift
+++ TodoList/Controller/CategoriaTableViewController.swift
@@ -6,84 +6,139 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoriaTableViewController: UITableViewController {
-
+    
+    //Array de categorias
+    var listaCategorias = [Categoria]()
+    
+    //Contexto
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    //Tabla con las categorias
+    @IBOutlet weak var tablaCategorias: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        //Cuando la app carga, llama al metodo leerTareas para recuperar los datos
+        let solicitud : NSFetchRequest<Categoria> = Categoria.fetchRequest()
+        leerCategorias(with: solicitud)
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    // Esta función guarda los cambios en el contexto de Core Data
+    func guardar(){
+        do{
+            // Se intenta guardar los cambios en el contexto
+            try context.save()
+        }catch{
+            print("Error guardando los cambios \(error)")
+        }
+        // Se actualiza la tabla de categorias para reflejar los cambios
+        self.tablaCategorias.reloadData()
     }
-
+    
+    // Esta función lee las tareas almacenadas en Core Data
+    func leerCategorias(with solicitud: NSFetchRequest<Categoria>){
+        do {
+            // Se intenta leer las tareas mediante la solicitud creada
+            listaCategorias = try context.fetch(solicitud)
+        }catch{
+            print("Error solicitando los datos \(error.localizedDescription)")
+        }
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return listaCategorias.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        //Celda reutilizable
+        let celda = tablaCategorias.dequeueReusableCell(withIdentifier: "CategoriaCell", for: indexPath)
+        
+        let categoria = listaCategorias[indexPath.row]
+        
+        celda.textLabel?.text = categoria.name
+        
+        return celda
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let accionEliminar = UIContextualAction(style: .normal, title: "Eliminar") { _, _, _ in
+            self.context.delete(self.listaCategorias[indexPath.row])
+            self.listaCategorias.remove(at: indexPath.row)
+            
+            //Guardamos los cambios en la base de datos
+            self.guardar()
+        }
+        //Color al eliminar elemento
+        accionEliminar.backgroundColor = .red
+        return UISwipeActionsConfiguration(actions: [accionEliminar])
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    //Nueva categoria
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        // Variable para almacenar el texto ingresado en el recuadro de la alerta
+        var nombreCategoria = UITextField()
+        
+        // Se crea la alerta
+        let alerta = UIAlertController(title: "Nueva categoria", message: "categoria", preferredStyle: .alert)
+        
+        // Acción para aceptar la creación de la nueva tarea
+        let accionAceptar = UIAlertAction(title: "Añadir", style: .default) { accion in
+            // Se crea una nueva categoria y se asigna el nombre ingresado en la alerta
+            let nuevaCategoria = Categoria(context: self.context)
+            nuevaCategoria.name = nombreCategoria.text
+            
+            // Se añade la nueva categoria a la lista de tareas
+            self.listaCategorias.append(nuevaCategoria)
+            
+            // Se guardan los cambios
+            self.guardar()
+        }
+        
+        // Se añade un campo de texto a la alerta para ingresar el nombre de la tarea
+        alerta.addTextField { textFieldAlerta in
+            textFieldAlerta.placeholder = "Escribe el nombre de la Categoria"
+            nombreCategoria = textFieldAlerta
+        }
+        
+        // Se añade la acción de aceptar a la alerta
+        alerta.addAction(accionAceptar)
+        
+        // Se presenta la alerta en pantalla
+        present(alerta, animated: true)
     }
-    */
+    
+    
+}
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
+extension CategoriaTableViewController : UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // Se crea una solicitud para leer entidades de tipo Categoria
+        let solicitud : NSFetchRequest<Categoria> =  Categoria.fetchRequest()
+        // Crea un predicado que filtre los resultados según el texto ingresado en la barra de búsqueda.
+        solicitud.predicate = NSPredicate(format: "name CONTAINS[cd] %@" , searchBar.text!)
+        
+        let ordenDeElementos = NSSortDescriptor(key: "name", ascending: true)
+        let resultadosOrdenados = listaCategorias.sorted(by: { ($0.name ?? "") < ($1.name ?? "") })
+        
+        leerCategorias(with: solicitud)
+        tablaCategorias.reloadData()
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    
+    //funcion para volver a la lista inicial de elementos
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // Se crea una solicitud para leer entidades de tipo Categoria
+        let solicitud : NSFetchRequest<Categoria> =  Categoria.fetchRequest()
+        if searchBar.text?.count == 0 {
+            leerCategorias(with: solicitud)
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+        tablaCategorias.reloadData()
     }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
